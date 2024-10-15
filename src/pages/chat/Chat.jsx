adc5d93ed6f4767cebbeaab0c4f02693347968f2
priyankr1 <160168368+priyankr1@users.noreply.github.com>
2024-10-15 22:49:09 +0530

    Update Chat.jsx

--- src/pages/chat/Chat.jsx
+++ src/pages/chat/Chat.jsx
@@ -6,25 +6,45 @@ import RightSidebar from '../../components/rightsidebar/RightSidebar';
 import Box from '@mui/material/Box';
 import { AppContext } from '../../context/AppContext';
 const Chat = () => {
-  const {chatData,userData}=useContext(AppContext)
-  const [loading,setLoading]=useState(true)
-  useEffect(()=>{
-      if(chatData && userData){
-        setLoading(false)
-      }
-  },[chatData,userData])
+  const { chatData, userData, userSelected } = useContext(AppContext)
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 700);
+
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+
+
+    // Function to handle screen resize
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 700);
+    };
+
+    // Add event listener for window resize
+    window.addEventListener('resize', handleResize);
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [window]);
+
+  useEffect(() => {
+    if (chatData && userData) {
+      setLoading(false)
+    }
+  }, [chatData, userData]);
   return (
     <Box className='chat'>
-    {
+      {
         loading
-        ?<p className='loading'>Loading....</p>: 
-        <Box className="chat-container">
-        <LeftSidebar/>
-        <ChatBox/>
-        <RightSidebar/>
-    </Box>
-    }
-       
+          ? <p className='loading'>Loading....</p> :
+          <Box className="chat-container">
+
+            {(!isMobile || !userSelected) && <LeftSidebar />}
+        {(!isMobile || userSelected)&&<ChatBox />}
+            <RightSidebar />
+          </Box>
+      }
+
     </Box>
   );
 }
