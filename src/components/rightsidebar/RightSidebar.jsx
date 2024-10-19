52ce602651115390fe798e5434fc9c70ef466213
priyankr1 <160168368+priyankr1@users.noreply.github.com>
2024-10-19 12:27:59 +0530

    Update RightSidebar.jsx

--- src/components/rightsidebar/RightSidebar.jsx
+++ src/components/rightsidebar/RightSidebar.jsx
@@ -9,40 +9,44 @@ import { AppContext } from '../../context/AppContext';
 const RightSidebar = () => {
   const {chatUser,messages}=useContext(AppContext)
   const [msgImages,setMsgImages]=useState([])
-  // useEffect(()=>{
-  //   let tempVar=[];
-  //   messages.map(()=>{
-  //     if(msg.image){
-  //       tempVar.push(msg.image)
-  //     }
-  //   })
-  //   console.log(tempVar)
-  // },[messages])
+  useEffect(()=>{
+    let tempVar=[];
+    console.log(messages)
+    messages.map((msg)=>{
+      if(msg.image){
+        tempVar.push(msg.image)
+      }
+    })
+   setMsgImages(tempVar)
+  },[messages])
   return chatUser?(
       <Box className='rs'>
         <Box className="rs-profile">
           <img className='prof-img' src={chatUser.userData.avatar} alt="" />
-          <h3>{chatUser.userData.name}<img className='dot' src={assets.green_dot} alt="" /></h3>
+          <h3>{chatUser.userData.name}{Date.now() - chatUser.userData.lastSeen<=70000?<img className='dot' src={assets.green_dot} alt="" />:null} </h3>
           <p>{chatUser.userData.bio}</p>
         </Box>
         <hr />
         <Box className='rs-media'>
           <p>Media</p>
           <Box>
-            <img src={assets.pic1} alt="" />
+            {
+              msgImages.map((url,index)=>(<img onClick={()=>window.open(url)} key={index} src={url} alt="" />)) }
+            {/* <img src={assets.pic1} alt="" />
             <img src={assets.pic2} alt="" />
             <img src={assets.pic3} alt="" />
             <img src={assets.pic4} alt="" />
             <img src={assets.pic5} alt="" />
-            <img src={assets.pic6} alt="" />
+            <img src={assets.pic6} alt="" /> */}
           </Box>
         </Box>
         <Button onClick={()=>logout()} variant="contained">Logout</Button>
         </Box>
-  ):
+  ):(
   <div className='rs'>
     <Button onClick={()=>logout()} variant="contained">Logout</Button>
   </div>
+  )
 }
 
 export default RightSidebar;
